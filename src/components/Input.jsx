eeb5b70d4ad43dd2ad822a6500945c9db5936744
Maxim Necrasov <maddmax22@yandex.ru>
2024-08-27 10:38:37 +0300

    added imput button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -13,6 +13,11 @@ const Input = () => {
     }
   };
 
+  const clickHandler = () => {
+    dispatch(addNewTask(text));
+    setText('');
+  };
+
   return (
     <div className="input__wrapper">
       <input
@@ -23,6 +28,9 @@ const Input = () => {
         onChange={({ target }) => setText(target.value)}
         onKeyDown={inputHandler}
       />
+      <button onClick={() => clickHandler()}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 20 20"><path fill="#555" d="M19.3 0a.7.7 0 0 1 .7.7v8.278c0 3.699-3 6.698-6.699 6.698l-10.996-.001 3.131 3.13a.7.7 0 0 1-.99.99l-4.24-4.241a.7.7 0 0 1 0-.99l4.241-4.241a.7.7 0 1 1 .99.99l-2.965 2.963h10.83A5.299 5.299 0 0 0 18.6 8.978V.7a.7.7 0 0 1 .7-.7Z"/></svg>
+      </button>
     </div>
   );
 };
